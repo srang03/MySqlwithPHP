731a8cc37dd42ce5e254f99d07e35a093adc9594
srang03 <srang_@naver.com>
2022-01-17 11:18:18 +0900

    commit

---
+++ mysql/220110/update_process.php
@@ -0,0 +1,20 @@
+<?php require_once('./lib/conn.php') ?>
+<?php
+if (isset($_POST['id']) && !empty($_POST['id'])){
+    $id = mysqli_real_escape_string($conn, $_POST['id']);
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $query = "UPDATE topic SET title='{$title}', description='{$description}' WHERE id = $id";
+
+    if($result = mysqli_query($conn, $query)){
+        header("Location: ./index.php?id=".$id);
+
+    }
+    else{
+        header("Location: ./common/systemError.php");
+    }
+
+}  
+ 
+
+?>
