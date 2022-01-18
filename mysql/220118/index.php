b59bab715cc0042d43e1c98108a531c00a1f2cbd
srang03 <srang_@naver.com>
2022-01-18 18:06:39 +0900

    commit220117

--- mysql/220118/index.php
+++ mysql/220118/index.php
@@ -12,10 +12,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Documen1t</title>
 </head> 
 <body>
     <h1><a href="./index.php">WEB</a></h1>
+    
     <ul>
         <?=$list?>
     </ul>
